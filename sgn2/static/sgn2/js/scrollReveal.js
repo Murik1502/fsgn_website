500037937b4affe41cde7dec538744d1e9d73479
nikitamurashka <nikita.murashka@gmail.com>
2024-07-21 23:29:44 +0300

    feat: appear animation

--- sgn2/static/sgn2/js/scrollReveal.js
+++ sgn2/static/sgn2/js/scrollReveal.js
@@ -1,45 +1,21 @@
-function isElementInViewport(el, percentVisible) {
-    const rect = el.getBoundingClientRect();
-    const windowHeight = window.innerHeight || document.documentElement.clientHeight;
-    const elementHeight = el.offsetHeight;
-    const visibleHeight = (elementHeight * percentVisible) / 100;
+document.addEventListener("DOMContentLoaded", function ()  {
+    const observerOptions = {
+        root: null,
+        rootMargin: '0px',
+        threshold: [0.1, 1.0]
+    };
 
-    return (
-        rect.top + visibleHeight >= 0 &&
-        rect.left >= 0 &&
-        rect.bottom - visibleHeight <= windowHeight &&
-        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-    );
-}
-
-function resetAnimation(elements, carousel) {
-    elements.forEach(element => {
-        element.classList.remove('visible');
-        void element.offsetWidth;
-    });
-    carousel.classList.remove('animate');
-    void carousel.offsetWidth;
-}
-
-function checkVisibility(containerSelector, elementsSelector, carouselSelector, percentVisible) {
-    const container = document.querySelector(containerSelector);
-    const elements = document.querySelectorAll(elementsSelector);
-    const carousel = document.querySelector(carouselSelector);
-
-    if (isElementInViewport(container, percentVisible)) {
-        elements.forEach(element => {
-            element.classList.add('visible');
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('visible');
+                entry.classList.remove('hidden');
+                observer.unobserve(entry.target);
+            }
         });
-        carousel.classList.add('animate');
-    } else {
-        resetAnimation(elements, carousel);
-    }
-}
+    }, observerOptions);
 
-function checkVisibilityHandler() {
-    checkVisibility('#container1 .company_carousel_wrap', '#container1 .company_layouts', '#container1 .company_carousel', 15);
-    checkVisibility('#container2 .company_carousel_wrap', '#container2 .company_layouts', '#container2 .company_carousel', 15);
-}
-
-window.addEventListener('scroll', checkVisibilityHandler);
-window.addEventListener('load', checkVisibilityHandler);
+    document.querySelectorAll('.element').forEach(element => {
+        observer.observe(element);
+    });
+});
