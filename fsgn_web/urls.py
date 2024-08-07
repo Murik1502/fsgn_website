f0d7384ab460e5458529b97bca81483276040b7a
Your Name <you@example.com>
2024-08-08 02:00:07 +0300

    add sgn1, sgn3, sgn4

--- fsgn_web/urls.py
+++ fsgn_web/urls.py
@@ -8,7 +8,10 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('fsgn.urls')),
-    path('sgn2/', include('sgn2.urls'))
+    path('sgn1/', include('sgn1.urls')),
+    path('sgn2/', include('sgn2.urls')),
+    path('sgn3/', include('sgn3.urls')),
+    path('sgn4/', include('sgn4.urls'))
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 if settings.DEBUG:
